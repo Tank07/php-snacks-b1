dc8af5f497c06727a00e8a81df6c38dc7d182c06
Tancredi Carluccio <tanc_@hotmail.it>
2022-05-12 01:42:35 +0200

    Snack 3 problemi, snack 4 finito

--- Snack 3/index.php
+++ Snack 3/index.php
@@ -1,2 +1,25 @@
 <!-- ## Snack 4
 Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+
+<?php
+
+    $arrayNumeri = [];
+    $arraySecchio = [];
+    $arrayUguali = [];
+
+    for ($i=0; count($arrayNumeri) <= 14 ; $i++) { 
+
+        $arraySecchio = rand(1,100);
+
+        if (in_array("$arraySecchio[$i]", $arrayNumeri))
+        {
+            $arrayUguali[] = + 1;
+        } else {
+            $arrayNumeri[] = $arraySecchio[$i];
+        }
+
+        echo "<p> $i)  $arrayNumeri[$i] </p>";
+
+    }
+
+?>
--- Snack 4/index.php
+++ Snack 4/index.php
@@ -1,3 +1,36 @@
-<!-- ## Snack 7
+<!-- ## Snack 4
 Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti
 scolastici. Stampare Nome, Cognome e la media dei voti di ogni alunno. -->
+
+<?php
+
+$aula = [
+    [
+        'nome' => 'Qui',
+        'cognome' => 'Verdi',
+        'voti' => [5,8,10,2],
+    ],
+    [
+        'nome' => 'Quo',
+        'cognome' => 'Rossi',
+        'voti' => [4,9,7,6],
+    ],
+    [
+        'nome' => 'Qua',
+        'cognome' => 'Smith',
+        'voti' => [7,3,9,1],
+    ],
+    [
+        'nome' => 'Pluto',
+        'cognome' => 'Pippo',
+        'voti' => [4,8,6,2],
+    ],
+];
+
+for ($i=0; $i < count($aula) ; $i++) { 
+    
+    echo $aula[$i]['nome'] . " " . $aula[$i]['cognome'] . " - Media voti: " . array_sum($aula[$i]['voti'])/4 . '<br>';
+
+}
+
+?>
